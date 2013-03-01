7928a015cfdde13389250574268d2521851e70d1
Erik Rydgren <erik@rydgrens.net>
2013-03-01 17:40:49 +0100

    Example how to add simple assembly caching client side

---
+++ Examples/ClientServer/Client/CachingAssemblyRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PluginFramework
+{
+  internal struct AssemblyCache
+  {
+    public DateTime TTL;
+    public byte[] Data;
+  }
+
+  public class CachingAssemblyRepository : MarshalByRefObject, IAssemblyRepository
+  {
+    IAssemblyRepository repository;
+    TimeSpan TTL;
+    Dictionary<string, AssemblyCache> cache = new Dictionary<string, AssemblyCache>();
+
+    public CachingAssemblyRepository(IAssemblyRepository repository, TimeSpan TTL)
+    {
+      this.repository = repository;
+      this.TTL = TTL;
+    }
+
+    public byte[] Get(string assemblyFullName)
+    {
+      AssemblyCache cached;
+      lock (this.cache)
+      {
+        if (this.cache.TryGetValue(assemblyFullName, out cached))
+        {
+          if (cached.TTL > DateTime.Now)
+            return cached.Data;
+        }
+      }
+
+      byte[] data = this.repository.Get(assemblyFullName);
+      if (data != null)
+      {
+        cached = new AssemblyCache();
+        cached.TTL = DateTime.Now + this.TTL;
+        cached.Data = data;
+        lock (this.cache)
+        {
+          this.cache[assemblyFullName] = cached;
+        }
+      }
+      return data;
+    }
+  }
+}
--- Examples/ClientServer/Client/Installers/PluginFrameworkInstaller.cs
+++ Examples/ClientServer/Client/Installers/PluginFrameworkInstaller.cs
@@ -11,9 +11,15 @@ namespace PluginFramework.Installers
     public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
     {
       container.Register(
-        Component.For<IPluginRepository>().LifestyleSingleton().ImplementedBy<RemotePluginRepository>(),
-        Component.For<IAssemblyRepository>().LifestyleSingleton().ImplementedBy<RemoteAssemblyRepository>(),
-        Component.For<PluginCreator>().LifestyleSingleton()
+        Component.For<IPluginRepository>().ImplementedBy<RemotePluginRepository>().LifestyleSingleton(),
+
+        Component.For<IAssemblyRepository>().ImplementedBy<RemoteAssemblyRepository>().LifestyleSingleton(),
+
+        Component.For<IAssemblyRepository>().Named("Caching").ImplementedBy<CachingAssemblyRepository>().LifestyleSingleton()
+          .DependsOn(new { TTL = TimeSpan.FromSeconds(10) }),
+
+        Component.For<Client>().ImplementedBy<Client>().LifestyleTransient()
+          .DependsOn(ServiceOverride.ForKey<IAssemblyRepository>().Eq("Caching"))
       );
     }
   }
