547ef49b9e2ba25385fbf308d2206e4288b23d6c
Erik Rydgren <erik@rydgrens.net>
2013-03-05 22:41:57 +0100

    Added copyright notice to source files and cleaned up used namespaces.

--- Examples/ClientServer/Client/Masstransit/MasstransitClient.cs
+++ Examples/ClientServer/Client/Masstransit/MasstransitClient.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using Castle.Core.Logging;
-using MassTransit;
-using PluginFramework.Command;
-
-namespace PluginFramework
+﻿// 
+// Copyright (c) 2013, Erik Rydgren, et al. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//  - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the distribution.
+//  - Neither the name of PluginFramework nor the names of its contributors may be used to endorse or promote products derived from this 
+//    software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ERIK RYDGREN OR OTHER CONTRIBUTORS 
+// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+//
+namespace PluginFramework.Examples.ClientServer
 {
+  using System;
+  using System.Collections.Generic;
+  using Castle.Core.Logging;
+  using MassTransit;
+
   class MasstransitClient : MarshalByRefObject, IPluginRepository, IAssemblyRepository
   {
     ILogger log;
@@ -31,7 +48,7 @@ namespace PluginFramework
           this.log.WarnFormat("Timeout requesting {0}", filter);
         });
 
-        cb.Handle<PluginFramework.Command.FindPluginResponse>((context, message) =>
+        cb.Handle<FindPluginResponse>((context, message) =>
         {
           foundPlugins = message.FoundPlugins;
           this.log.DebugFormat("Found {0} plugins for {1}", foundPlugins.Length, filter);
